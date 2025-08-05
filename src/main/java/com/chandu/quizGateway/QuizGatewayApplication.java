7e099b00a36d269c57c5c2b778d005c391fb4d26
PakkurthiChandu <21mh1a4241@acoe.edu.in>
2025-08-05 18:17:17 +0530

    first commit

---
+++ src/main/java/com/chandu/quizGateway/QuizGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.chandu.quizGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chandu/quizGateway/QuizGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandu.quizGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
